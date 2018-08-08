86d25a62ff33861b5dfd984772f5d651b60e9d33
Simon Ellett <>
2018-08-08 22:27:10 +0100

    further changes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
+//import registerServiceWorker from "./registerServiceWorker";
 
 import { BrowserRouter } from "react-router-dom";
 
@@ -10,9 +10,9 @@ import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.js";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
   </BrowserRouter>,
   document.getElementById("root")
 );
-registerServiceWorker();
+//registerServiceWorker();
