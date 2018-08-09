9f819f605444cfd7a7ebfb04d85c9ba7c529c807
Simon Ellett <>
2018-08-09 19:13:36 +0100

    further work

---
+++ src/components/pages.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+export const Home = () => (
+  <div>
+    <h1>Home</h1>
+  </div>
+);
+
+export const RiderInfo = () => (
+  <div>
+    <h1>Rider Info</h1>
+  </div>
+);
+
+export const Committee = () => (
+  <div>
+    <h1>Committee</h1>
+  </div>
+);
+
+export const Sponsors = () => (
+  <div>
+    <h1>Sponsors</h1>
+  </div>
+);
--- src/index.js
+++ src/index.js
@@ -3,8 +3,18 @@ import ReactDOM from "react-dom";
 import "./index.css";
 //import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
-//import "bootstrap/dist/css/bootstrap.css";
-import Header from "./components/header";
 
-ReactDOM.render(<Header />, document.getElementById("root"));
+import Header from "./components/header";
+import { BrowserRouter } from "react-router-dom";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
+
+ReactDOM.render(
+  <BrowserRouter>
+    {/* <App /> */}
+    <Header />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 registerServiceWorker();
