a99416207f532a14071392839ec7ad739460c547
Simon Ellett <>
2019-01-11 20:24:17 +0000

    updated to add poster and some resizing

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import DocumentTitle from "react-document-title";
-//import Logo from "../images/south-west-motocross-club-logo.jpg";
 import Logo from "../images/swmx_logo.png";
+import LatestPosters from "../images/latestPoster.png";
 import Facebook from "../components/facebookPlugin";
 
 class Home extends Component {
@@ -10,20 +10,27 @@ class Home extends Component {
       <DocumentTitle title="Home | SWMX">
         <div>
           <img
-            className="img-fluid margin-bottom-20"
+            id="logo"
+            className="img-fluid"
             src={Logo}
             alt="South West Motocross Club"
           />
 
-          <div className="container-fluid margin-top-25 margin-bottom-20">
+          <img
+            id="latestPoster"
+            className="img-fluid"
+            src={LatestPosters}
+            alt="Latest Poster"
+          />
+
+          <div id="home-body" className="container-fluid">
             <div className="row">
-              <div className="col-sm-6 margin-top-25 d-flex flex-column">
+              <div className="button-column col-sm-6 d-flex flex-column">
                 <p>
                   Memberships are now available! For further details about
                   membership and other club information such as fixtures and
                   results
                 </p>
-
                 <p className="mt-auto">
                   <a className="btn btn-info" href="/RiderInformation">
                     View Rider Information
@@ -31,7 +38,7 @@ class Home extends Component {
                 </p>
               </div>
 
-              <div className="col-sm-6 margin-top-25 d-flex flex-column">
+              <div className="button-column col-sm-6 d-flex flex-column">
                 <p>Please take a moment to view this year's sponsors</p>
 
                 <p className="mt-auto">
@@ -41,7 +48,8 @@ class Home extends Component {
                 </p>
               </div>
             </div>
-            <div className="row margin-top-60">
+
+            <div id="latest-events" className="row">
               <h2 className="mx-auto">Latest Event / Facebook News</h2>
               <Facebook
                 tabs="events,timeline"
